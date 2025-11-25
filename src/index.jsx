0048b9295b63458b9b3c6b2bb069af4231de2c69
hd789789789 <65296761+hd789789789@users.noreply.github.com>
2025-11-25 15:35:21 +0700

    Add Vietnamese translation

--- src/index.jsx
+++ src/index.jsx
@@ -1,62 +1,60 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-import {
-  APP_INIT_ERROR,
-  APP_READY,
-  initialize,
-  mergeConfig,
-  subscribe,
-} from '@edx/frontend-platform';
-import {
-  AppProvider,
-  ErrorPage,
-} from '@edx/frontend-platform/react';
+import { APP_INIT_ERROR, APP_READY, initialize, mergeConfig, subscribe } from "@edx/frontend-platform";
+import { AppProvider, ErrorPage } from "@edx/frontend-platform/react";
 
-import React, { StrictMode } from 'react';
+import React, { StrictMode } from "react";
 // eslint-disable-next-line import/no-unresolved
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-import Header from '@edx/frontend-component-header';
-import { FooterSlot } from '@edx/frontend-component-footer';
+import Header from "@edx/frontend-component-header";
+import { FooterSlot } from "@edx/frontend-component-footer";
 
-import messages from './i18n';
-import configureStore from './data/configureStore';
+import messages from "./i18n";
+import configureStore from "./data/configureStore";
 
-import './index.scss';
-import Head from './head/Head';
+import "./index.scss";
+import Head from "./head/Head";
 
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from "./routes/AppRoutes";
 
-const rootNode = createRoot(document.getElementById('root'));
+import { TextReplacerProvider } from "./generic/text-replacer/TextReplacer";
+
+const rootNode = createRoot(document.getElementById("root"));
 subscribe(APP_READY, () => {
-  rootNode.render(
-    <StrictMode>
-      <AppProvider store={configureStore()}>
-        <Head />
-        <Header />
-        <main id="main">
-          <AppRoutes />
-        </main>
-        <FooterSlot />
-      </AppProvider>
-    </StrictMode>,
-  );
+    rootNode.render(
+        <StrictMode>
+            <AppProvider store={configureStore()}>
+                <TextReplacerProvider>
+                    <Head />
+                    <Header />
+                    <main id="main">
+                        <AppRoutes />
+                    </main>
+                    <FooterSlot />
+                </TextReplacerProvider>
+            </AppProvider>
+        </StrictMode>
+    );
 });
 
 subscribe(APP_INIT_ERROR, (error) => {
-  rootNode.render(<ErrorPage message={error.message} />);
+    rootNode.render(<ErrorPage message={error.message} />);
 });
 
 initialize({
-  messages,
-  hydrateAuthenticatedUser: true,
-  handlers: {
-    config: () => {
-      mergeConfig({
-        COLLECT_YEAR_OF_BIRTH: process.env.COLLECT_YEAR_OF_BIRTH,
-        ENABLE_SKILLS_BUILDER_PROFILE: process.env.ENABLE_SKILLS_BUILDER_PROFILE,
-      }, 'App loadConfig override handler');
+    messages,
+    hydrateAuthenticatedUser: true,
+    handlers: {
+        config: () => {
+            mergeConfig(
+                {
+                    COLLECT_YEAR_OF_BIRTH: process.env.COLLECT_YEAR_OF_BIRTH,
+                    ENABLE_SKILLS_BUILDER_PROFILE: process.env.ENABLE_SKILLS_BUILDER_PROFILE,
+                },
+                "App loadConfig override handler"
+            );
+        },
     },
-  },
 });
